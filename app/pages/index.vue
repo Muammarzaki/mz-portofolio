466e6b6f63d18dafe77e1a668fe66db6d6b77704
muammarzaki <muammarzaki2004@gmail.com>
2025-08-08 22:39:20 +0700

    feat: update a homepage background with gradient and improved image styling

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,6 +3,10 @@
 </script>
 
 <template>
-  <Navbar/>
-  <img src="/close-up-coffee-maker-black-background.jpg" alt="java icon">
+  <div class="bg-gradient-to-t from-[#0A0A0A] via-[#050505] to-[#030303]">
+    <div
+        class="bg-[url('/close-up-coffee-maker-black-background.jpg')] bg-cover bg-bottom lg:[background-position-x:calc(100%+1rem)] lg:[background-position-y:calc(100%+3.5rem)]  min-h-screen ">
+      <Navbar/>
+    </div>
+  </div>
 </template>
