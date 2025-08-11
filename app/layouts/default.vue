e1018da7c1e14ad626dde858a1ad183a439f7011
muammarzaki <muammarzaki2004@gmail.com>
2025-08-11 23:50:21 +0700

    style: update padding in the default layout for improved spacing consistency

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-[calc(100vh-20px)]">
     <Navbar/>
-    <main class="flex-grow lg:pb-12">
+    <main class="flex-grow lg:pb-4">
       <slot/>
     </main>
   </div>
