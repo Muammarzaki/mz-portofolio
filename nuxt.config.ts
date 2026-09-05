f17d9d0a143a0075e573198ff6f6d0e090cea6e0
muammarzaki <muammarzaki2004@gmail.com>
2026-09-05 10:31:32 +0700

    feat: improve css size optimize

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
             tailwindcss()
         ]
     },
+    features: {
+        inlineStyles: true
+    },
     experimental: {
         payloadExtraction: true
     },
